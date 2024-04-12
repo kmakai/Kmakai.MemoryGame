cb7d30b8d7d033c4e09b206df623399736bb9201
kmakai <makaikwag@gmail.com>
2024-04-11 21:10:56 -0400

    Add project files.

---
+++ Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace Kmakai.MemoryGame.Client.Models;
+
+public class Card
+{
+    public string Image { get; set; } = "";
+    public string Name { get; set; } = "";
+    public bool IsFlipped { get; set; } = false;
+    public bool IsMatched { get; set; } = false;
+}
---
+++ Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/CardList.cs
@@ -0,0 +1,30 @@
+﻿namespace Kmakai.MemoryGame.Client.Models;
+
+public class CardList
+{
+    private static List<Card> CardsList = new List<Card>()
+    {
+        new Card { Image = "https://via.placeholder.com/150", Name = "one", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "two", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "three", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "four", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "five", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "6", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "7", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "8", IsFlipped = false, IsMatched = false },
+        new Card { Image = "https://via.placeholder.com/150", Name = "9", IsFlipped = false, IsMatched = false },
+
+    };
+
+    public static List<Card> GetCards()
+    {
+        return CardsList.ToList();
+    }
+
+    public static List<Card> GetRandomCards(int n)
+    {
+        return CardsList.OrderBy(x => Guid.NewGuid()).Take(n).ToList();
+    }
+
+
+}
