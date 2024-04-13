74444f3bc149b1339ceb860d90ad535950800ee1
kmakai <makaikwag@gmail.com>
2024-04-12 20:42:52 -0400

    Ui, and card matching functionality done.

--- Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/CardList.cs
+++ Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/CardList.cs
@@ -4,15 +4,31 @@ public class CardList
 {
     private static List<Card> CardsList = new List<Card>()
     {
-        new Card { Image = "https://via.placeholder.com/150", Name = "one", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "two", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "three", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "four", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "five", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "6", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "7", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "8", IsFlipped = false, IsMatched = false },
-        new Card { Image = "https://via.placeholder.com/150", Name = "9", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/choji.png", Name = "choji", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/gaara.png", Name = "gaara", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/guy.png", Name = "guy", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/hinata.png", Name = "hinata", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/ino.png", Name = "ino", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/jiraya.png", Name = "jiraya", IsFlipped = false, IsMatched = false },        
+        new Card { Image = "images/kakashi.png", Name = "kakashi", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/kiba.png", Name = "kiba", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/kushina.png", Name = "kushina", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/minato.png", Name = "minato", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/naruto.png", Name = "naruto", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/narutoandsasuke.png", Name = "narutoandsasuke", IsFlipped = false, IsMatched = false },        
+        new Card { Image = "images/neji.png", Name = "neji", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/obito.png", Name = "obito", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/orochimaru.png", Name = "orochimaru", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/rocklee.png", Name = "rocklee", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/sakura.png", Name = "sakura", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/sasuke.png", Name = "sasuke", IsFlipped = false, IsMatched = false },        
+        new Card { Image = "images/shino.png", Name = "shino", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/kankuro.png", Name = "kankuro", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/shikamaru.png", Name = "shikamaru", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/temari.png", Name = "temari", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/tenten.png", Name = "tenten", IsFlipped = false, IsMatched = false },
+        new Card { Image = "images/tsunade.png", Name = "tsunade", IsFlipped = false, IsMatched = false },
+
 
     };
 
---
+++ Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/GameMode.cs
@@ -0,0 +1,30 @@
+﻿namespace Kmakai.MemoryGame.Client.Models;
+
+public enum GameMode
+{
+    Normal = 1,
+    RaceTime = 2,
+    Moves = 3
+}
+
+public enum GameDifficulty
+{
+    Easy = 1,
+    Normal = 2,
+    Hard = 3
+}
+
+public static class GameDifficultyExtensions
+{
+    public static int GetCardsNumber(this GameDifficulty difficulty)
+    {
+        return difficulty switch
+        {
+            GameDifficulty.Easy => 8,
+            GameDifficulty.Normal => 12,
+            GameDifficulty.Hard => 16,
+            _ => 12
+        };
+
+    }
+}
