3e9b52fe5da39631677b0cb83e3f713cf8eb85cc
kmakai <makaikwag@gmail.com>
2024-04-16 17:08:38 -0400

    game funtionality finished, will work on saving to db classes added for entities

---
+++ Kmakai.MemoryGame/Kmakai.MemoryGame.Client/Models/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace Kmakai.MemoryGame.Client.Models;
+
+public class NormalGame
+{
+    public int Id { get; set; }
+    public GameDifficulty GameDifficulty { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+
+public class RaceGame
+{
+    public int Id { get; set; }
+    public GameDifficulty GameDifficulty { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Duration { get; set; }
+    
+    public int Score { get; set; }
+    public int TimeLimit { get; set; }
+}
+
+public class MovesGame
+{
+    public int Id { get; set; }
+    public GameDifficulty GameDifficulty { get; set; }
+    public DateTime Date { get; set; }
+    public int score { get; set; }
+    public int Moves { get; set; }
+    public int MovesLimit { get; set; }
+}
