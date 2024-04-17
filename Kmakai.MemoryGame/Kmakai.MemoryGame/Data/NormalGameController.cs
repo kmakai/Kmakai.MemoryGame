fa1415d3d07edb6f3b7e25c8f63738c3a1057ec8
kmakai <makaikwag@gmail.com>
2024-04-17 19:10:31 -0400

    saving normal game completed working on saving racegame

--- Kmakai.MemoryGame/Kmakai.MemoryGame/Data/NormalGameController.cs
+++ Kmakai.MemoryGame/Kmakai.MemoryGame/Data/NormalGameController.cs
@@ -1,9 +1,7 @@
 ﻿using Kmakai.MemoryGame.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Kmakai.MemoryGame.Data;
 
---
+++ Kmakai.MemoryGame/Kmakai.MemoryGame/Data/RaceGameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Kmakai.MemoryGame.Client.Models;
+
+
+namespace Kmakai.MemoryGame.Data;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RaceGameController : ControllerBase
+{
+    private readonly GameContext _context;
+    public RaceGameController(GameContext context)
+    {
+        _context = context;
+    }
+    // GET: api/<RaceGameController>
+    [HttpGet]
+    public async Task<List<RaceGame>> Get()
+    {
+        return await _context.RaceGames.ToListAsync();
+    }
+
+    // GET api/<RaceGameController>/5
+    [HttpGet("{id}")]
+    public async Task<RaceGame?> Get(int id)
+    {
+        return await _context.RaceGames.FindAsync(id);
+    }
+
+    // POST api/<RaceGameController>
+    [HttpPost]
+    public async Task Post([FromBody] RaceGame value)
+    {
+        await _context.RaceGames.AddAsync(value);
+        await _context.SaveChangesAsync();
+    }
+
+    // DELETE api/<RaceGameController>/5
+    [HttpDelete("{id}")]
+    public async Task Delete(int id)
+    {
+        var game = await _context.RaceGames.FindAsync(id);
+        if (game == null)
+        {
+            return;
+        }
+        _context.RaceGames.Remove(game);
+        await _context.SaveChangesAsync();
+    }
+}
